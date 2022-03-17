53721fcfa12a346a2547cf15d2a79549a80c0111
CrimsonCoder42 <nostro37@gmail.com>
2022-03-17 12:25:12 -0400

    5:25:02

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,3 +1,5 @@
+// 5:15:51
+
 const express = require('express');
 const path = require('path');
 
@@ -8,9 +10,9 @@ const port = 8080;
 app.use(express.static('./public'))
 
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+// })
 
 app.all('*', (req, res) => {
     res.status(404).send('resource not found')
