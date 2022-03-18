f314d310506caf09d63ce7c9d647f30d0e82c617
CrimsonCoder42 <nostro37@gmail.com>
2022-03-18 11:01:50 -0400

    6:23:46

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,62 +1,22 @@
-const express = require('express'); 
+const express = require('express');
 const app = express(); 
-const { products } = require('./data');
+const logger = require('./logger');
 
 const port = 8080;
 
+// req => middleware res
 
-app.get('/', (req, res) => {
-    res.send("<h1> Home Page </h1> <a href='/api/products'>products</a>")
+
+app.get("/", logger ,(req, res) => {
+    res.send("Home");
 })
 
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const {id, name, image} = product;
-        return {id, name, image}
-    })
-    res.json(newProducts)
+app.get("/about", logger , (req, res) => {
+    res.send("About"); 
 })
 
-app.get('/api/products/:productID', (req, res) => {
-    const {productID} = req.params;
-    console.log(req.params)
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if (!singleProduct) {
-        return res.status(404).send('Product Does Not Exist')
-    }
-    return res.json(singleProduct);
-})
-
-
-app.get('/api/products/:productID/reviews/:reviewID', (req,res) => {
-    console.log(req.params);
-    res.send("hello world")
-} )
-
-
-app.get('/api/v1/query', (req, res) => {
-    const {search, limit} = req.query
-    let sortedProducts = [ ...products ]
-
-    if(search){
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if(sortedProducts.length < 1) {
-        //res.status(200).send('no products matched your search. ')
-        return res.status(200).json({ success:true, data: [] })
-    }
-    res.status(200).json(sortedProducts)
-    
-})
 
 
 app.listen(port, () => {
-    console.log(`app running on localhost${port}`)
+    console.log(`Server is listening on localhost:${port}.`);
 })
-
-//5:32:34
