81309f73d81e21f7092e626dbaf7139b79cef37a
CrimsonCoder42 <nostro37@gmail.com>
2022-03-19 10:15:00 -0400

    6:54:42

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,31 +1,17 @@
 const express = require('express');
 const app = express(); 
-const logger = require('./logger');
-const authorize = require('./authorize');
-
-const port = 8080;
-
-// req => middleware res
-
-app.use(express.static('./public'));
+let { people } = require('./data');
+const port = 8080
 
 
-app.get("/", (req, res) => {
-    res.send("Home");
-})
+// static assets 
 
-app.get("/about", (req, res) => {
-    res.send("About"); 
-})
+app.use(express.static('./methods-public'))
 
-app.get("/api/products", (req, res) => {
-    res.send("Home");
-})
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success:true, data:people})
+} )
 
-app.get("/api/items", (req, res) => {
-    console.log(req.user);
-    res.send("About"); 
-})
 
 app.listen(port, () => {
     console.log(`Server is listening on localhost:${port}.`);
