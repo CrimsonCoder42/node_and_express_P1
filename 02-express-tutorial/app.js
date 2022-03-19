10ee66d891b29c8c1c82b7469446bce0aa4604b1
CrimsonCoder42 <nostro37@gmail.com>
2022-03-19 10:21:58 -0400

    6:58:52

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -8,10 +8,17 @@ const port = 8080
 
 app.use(express.static('./methods-public'))
 
+
+app.use(express.url)
+
+
 app.get('/api/people', (req, res) => {
     res.status(200).json({success:true, data:people})
 } )
 
+app.post('/login', (req, res) => {
+    res.send('POST')
+})
 
 app.listen(port, () => {
     console.log(`Server is listening on localhost:${port}.`);
