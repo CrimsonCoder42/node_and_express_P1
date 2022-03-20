40033da68dfffb61467ec5e1cce434da68e45804
CrimsonCoder42 <nostro37@gmail.com>
2022-03-20 13:37:05 -0400

    8:13:53

---
+++ 02-express-tutorial/routes/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const auth = express.Router();
+
+
+auth.post('/', (req, res) => {
+    const {name} = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide credentials.')
+})
+
+
+module.exports = auth;
